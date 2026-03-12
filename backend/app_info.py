3ea03f442fe92d3c48a20ee835f1c505fd4b86f5
Zhdanova Ekaterina <zhdanova.ekaterina.95@yandex.ru>
2026-03-12 23:10:40 +0300

    changelog

--- backend/app_info.py
+++ backend/app_info.py
@@ -1,4 +1,4 @@
 """Application-level constants: version and external links."""
 
-APP_VERSION = "0.2.0"
+APP_VERSION = "0.3.0"
 GUIDE_URL = "https://github.com/zhdanovaekaterina/menutor/blob/master/docs/user_guide.md"
